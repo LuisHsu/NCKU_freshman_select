f0f0c82191882d31a5ab570d6cc8d06fd0fd2d2e
LuisHsu <luis8404272@gmail.com>
2014-08-04 20:21:58 +0800

    1.1
    Add LazyLink and appendix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var page=1;
+var Tutor_StartPage=0;
+var Tutor_MinPage=0;
+var Tutor_MaxPage=6;
 var TimerCount=0;
 
 function left_hover(tog){
@@ -10,10 +12,10 @@ function left_hover(tog){
 }
 
 function left_Click(){
-	page-=1;
+	Tutor_StartPage-=1;
 	var rightTimer=setInterval(function (){if(TimerCount<80){document.getElementById("Tutorial_Frame").contentWindow.postMessage("left", "*");TimerCount+=1;}else{clearInterval(rightTimer);TimerCount=0;}},1);
-	if (page==1){document.getElementById("left_Button").style.visibility="hidden";}
-	if (page==4){document.getElementById("right_Button").style.visibility="visible";}
+	if (Tutor_StartPage==Tutor_MinPage){document.getElementById("left_Button").style.visibility="hidden";}
+	if (Tutor_StartPage==(Tutor_MaxPage-1)){document.getElementById("right_Button").style.visibility="visible";}
 }
 
 function right_hover(tog){
@@ -25,8 +27,8 @@ function right_hover(tog){
 }
 
 function right_Click(){
-	page+=1;
+	Tutor_StartPage+=1;
 	var rightTimer=setInterval(function (){if(TimerCount<80){document.getElementById("Tutorial_Frame").contentWindow.postMessage("right", "*");TimerCount+=1;}else{clearInterval(rightTimer);TimerCount=0;}},1);
-	if (page==2){document.getElementById("left_Button").style.visibility="visible";}
-	if (page==5){document.getElementById("right_Button").style.visibility="hidden";}
+	if (Tutor_StartPage==(Tutor_MinPage+1)){document.getElementById("left_Button").style.visibility="visible";}
+	if (Tutor_StartPage==Tutor_MaxPage){document.getElementById("right_Button").style.visibility="hidden";}
 }
