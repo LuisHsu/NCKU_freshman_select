4a02ebcb3533019b5da23980418f25992d7313fe
LuisHsu <luis8404272@gmail.com>
2014-08-06 16:39:05 +0800

    1.6-2
    test analytics

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var Tutor_MinPage=0;
 var Tutor_MaxPage=7;
 var TimerCount=0;
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53567358-1', 'auto');
+  ga('send', 'pageview');
+
 function left_hover(tog){
 	if(tog==1){
 		document.getElementById("left_Button").style.backgroundImage="url('Resources/left_button_hover.png')";
